06deeafc4d4c2154da1eec0af80118dd11d149c8
WEBmen <shalcer13@mail.ru>
2021-11-08 16:43:12 +0300

    Create Schedule

--- web-chtivo/src/components/templates/HomePages/Schedule.vue
+++ web-chtivo/src/components/templates/HomePages/Schedule.vue
@@ -1,14 +1,42 @@
 <template>
-  Schedule
+  <q-layout view="lHh Lpr lFf">
+    <Header
+      down-side-bar="true"
+      user-settings="/Settings"
+      name="Test"
+      avatar="account_circle"
+      @getSelectedDay="getDay"
+    />
+    <ContentLayout>
+      <CardContainer :name="dayOfTheWeek" />
+    </ContentLayout>
+  </q-layout>
 </template>
 
 <script>
-import {defineComponent} from "vue";
-
+import {defineComponent, ref} from "vue";
+import Header from "components/organism/Header";
+import ContentLayout from "components/templates/ContentTemplate/ContentLayout";
+import CardContainer from "components/organism/CardContainer";
 export default defineComponent({
-  name: 'Schedule',
+  name: 'ScheduleNormal',
   components: {
+    CardContainer,
+    Header,
+    ContentLayout
   },
+  setup(){
+
+    const dayOfTheWeek = ref("Понедельник")
+    return{
+      dayOfTheWeek,
+
+      getDay(getData){
+        dayOfTheWeek.value = getData
+        console.log(dayOfTheWeek.value)
+      }
+    }
+  }
 })
 </script>
 
